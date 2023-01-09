4fad87b3eef20dd903b5076101a3f5eb22d43e6b
jschuyl <schuylertjed@gmail.com>
2023-01-09 14:16:49 -0700

    experimenting with uuid

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,4 +1,5 @@
 import { openDB } from 'idb';
+import { v4 as uuid } from 'uuid'
 
 const initdb = async () =>
   openDB('jate', 1, {
@@ -12,14 +13,28 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// TODO: Add logic to a method that accepts some content and adds it to the database //Completed
 export const putDb = async (content) => {
-  console.log('Poat data to DB initiated');
-  const postDb = await openDB('contact')
-  
-  console.error('Data Saved to DB');
+  console.log('Put data to DB initiated');
+  const contactDb = await openDB('jate', 1);
+  const tx = contactDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate')
+  const request = store.put({id: uuid, value: content});
+  const result = await request
+  console.log('Put data to DB successfully', result)  
+  console.error('putDB not implemented');
 }
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// TODO: Add logic for a method that gets all the content from the database //Completed
+export const getDb = async () => {
+  console.log('Data get from DB initiated');
+  const contactDb = await openDB('jate', 1);
+  const tx = contactDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll(); 
+  const result = await request;
+  console.log('Data got', result);
+  console.error('getDb not implemented');
+  return result;
+};
 
 initdb();
