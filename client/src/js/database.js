bfef1bfd317d1bfd67633ee781358ea9423c7340
jschuyl <schuylertjed@gmail.com>
2023-01-09 14:49:01 -0700

    ran build getDb isn't being implemented for some reason

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,5 +1,4 @@
 import { openDB } from 'idb';
-import { v4 as uuid } from 'uuid'
 
 const initdb = async () =>
   openDB('jate', 1, {
@@ -16,10 +15,10 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database //Completed
 export const putDb = async (content) => {
   console.log('Put data to DB initiated');
-  const contactDb = await openDB('jate', 1);
-  const tx = contactDb.transaction('jate', 'readwrite');
+  const accessDb = await openDB('jate', 1);
+  const tx = accessDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate')
-  const request = store.put({id: uuid, value: content});
+  const request = store.put({id: 1, value: content});
   const result = await request
   console.log('Put data to DB successfully', result)  
   console.error('putDB not implemented');
@@ -27,10 +26,10 @@ export const putDb = async (content) => {
 // TODO: Add logic for a method that gets all the content from the database //Completed
 export const getDb = async () => {
   console.log('Data get from DB initiated');
-  const contactDb = await openDB('jate', 1);
-  const tx = contactDb.transaction('jate', 'readonly');
+  const accessDb = await openDB('jate', 1);
+  const tx = accessDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.getAll(); 
+  const request = store.get(1); 
   const result = await request;
   console.log('Data got', result);
   console.error('getDb not implemented');
